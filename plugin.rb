83a222608e5354c13439e20dfcdb568cbede4c2d
Ahmed <ahmedgagan3@gmail.com>
2020-04-08 14:55:55 +0530

    plugin details added in plugin.rb file

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,18 @@
+# name: custom trust level
+# about: Adds a few custom trust levels capabilities in Discourse
+# version: 0.1.0
+# authors: Ahmed Gagan(Ahmedgagan), Faizan Gagan(fzngagan)
+# url: https://github.com/Ahmedgagan/discourse-custom-trust-level
+
 
 enabled_site_setting :custom_trust_level_enabled
 after_initialize do
-
   module ModifyCanCreate
+
     def can_create_post_on_topic?(topic)
       is_admin?||is_moderator?||( super && user.trust_level >= SiteSetting.can_create_post_on_topic_min_trust_level)
     end
+
     def can_edit_topic?(topic)
       return false if (
         !SiteSetting.can_tl3_edit_topics
@@ -17,6 +24,7 @@ after_initialize do
       )
       super
     end
+
     def can_reply_as_new_topic?(topic)
       (authenticated? && topic && SiteSetting.can_tl0_reply_as_new_topic && user.has_trust_level?(TrustLevel[0])) || super
     end
