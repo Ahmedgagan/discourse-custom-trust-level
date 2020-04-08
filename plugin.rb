31c399aa5ec11fb2b5e08b62d44cfe5ad2ccdbfe
Ahmed <ahmedgagan3@gmail.com>
2020-04-08 22:26:41 +0530

    Added new setting can_invite_to_topic_min_trust_level

--- plugin.rb
+++ plugin.rb
@@ -28,9 +28,39 @@ after_initialize do
     def can_reply_as_new_topic?(topic)
       (authenticated? && topic && SiteSetting.can_tl0_reply_as_new_topic && user.has_trust_level?(TrustLevel[0])) || super
     end
+
+    def can_invite_to?(object, groups = nil)
+
+      return false unless authenticated?
+      is_topic = object.is_a?(Topic)
+      return true if is_admin? && !is_topic
+      return false if (SiteSetting.max_invites_per_day.to_i == 0 && !is_staff?)
+      return false unless can_see?(object)
+      return false if groups.present?
+
+      if is_topic
+        if object.private_message?
+          return true if is_admin?
+          return false unless SiteSetting.enable_personal_messages?
+          return false if object.reached_recipients_limit? && !is_staff?
+        end
+
+        if (category = object.category) && category.read_restricted
+          if (groups = category.groups&.where(automatic: false))&.any?
+            return groups.any? { |g| can_edit_group?(g) } ? true : false
+          else
+            return false
+          end
+        end
+      end
+
+
+      user.has_trust_level?(SiteSetting.can_invite_to_topic_min_trust_level)
+    end
   end
 
   class ::Guardian
     prepend ModifyCanCreate if SiteSetting.custom_trust_level_enabled
+    
   end
 end
