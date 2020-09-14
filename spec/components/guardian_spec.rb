06c402344a7b3a32a3261fa247154f5d4d21aabb
Ahmed <ahmedgagan6@gmail.com>
2020-09-14 20:27:31 +0530

    rspec tests added

--- spec/components/guardian_spec.rb
+++ spec/components/guardian_spec.rb
@@ -8,14 +8,21 @@ describe Guardian do
   let(:trust_level_1) { build(:user, trust_level: 1) }
   let(:trust_level_2) { build(:user, trust_level: 2) }
   let(:trust_level_3) { build(:user, trust_level: 3) }
-  let(:trust_level_4) { build(:user, trust_level: 4) }
-  let(:topic) { Fabricate(:topic) }
+  let(:admin) { build(:admin) }
+  let(:moderator) { build(:moderator) }
+  let(:topic) { Fabricate(:topic, posts_count: 10) }
 
   before { SiteSetting.custom_trust_level_enabled = true }
 
   describe "can_create_post_on_topic?" do
     before do
       SiteSetting.csl_can_create_post_on_topic_min_trust_level = 1
+      SiteSetting.csl_numbers_of_replies_upto_20_posts = 2
+      SiteSetting.csl_numbers_of_replies_upto_50_posts = 3
+      SiteSetting.csl_numbers_of_replies_above_50_posts = 5
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 11)
+      topic.posts << Fabricate(:post, user: admin, post_number: 13)
+      topic.posts << Fabricate(:post, user: admin, post_number: 14)
     end
 
     it 'returns true if user has trust_level specified in site setting' do
@@ -24,6 +31,53 @@ describe Guardian do
 
     it 'returns false if user doesnt have has trust_level specified in site setting' do
       expect(Guardian.new(trust_level_0).can_create_post_on_topic?(topic)).to eq(false)
+    end
+
+    it 'returns true if users has posts less than site setting csl_numbers_of_replies_upto_20_posts' do
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(true)
+    end
+
+    it 'returns true if users has posts less than site setting csl_numbers_of_replies_upto_50_posts' do
+      topic.posts_count = 21
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 22)
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(true)
+    end
+
+    it 'returns true if users has posts less than site setting csl_numbers_of_replies_above_50_posts' do
+      topic.posts_count = 51
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 52)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 53)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 54)
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(true)
+    end
+
+    it 'returns false if users have posts count equal to site setting csl_numbers_of_replies_upto_20_posts' do
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 12)
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(false)
+    end
+
+    it 'returns false if users have posts count equal to site setting csl_numbers_of_replies_upto_50_posts' do
+      topic.posts_count = 21
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 22)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 23)
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(false)
+    end
+
+    it 'returns false if users have posts count equal to site setting csl_numbers_of_replies_above_50_posts' do
+      topic.posts_count = 51
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 52)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 53)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 54)
+      topic.posts << Fabricate(:post, user: trust_level_1, post_number: 55)
+      expect(Guardian.new(trust_level_1).can_create_post_on_topic?(topic)).to eq(false)
+    end
+
+    it 'returns true if user is admin' do
+      expect(Guardian.new(admin).can_create_post_on_topic?(topic)).to eq(true)
+    end
+
+    it 'returns true if user is admin' do
+      expect(Guardian.new(moderator).can_create_post_on_topic?(topic)).to eq(true)
     end
   end
 
